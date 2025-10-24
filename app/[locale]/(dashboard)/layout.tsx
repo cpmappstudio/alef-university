986dbbb66d0b23d1fb04bfe4cea30f106fd2ad37
Juan Camilo Narváez <ulvenforst@gmail.com>
2025-10-24 09:01:41 -0500

    some minor changes to visuals

--- app/[locale]/(dashboard)/layout.tsx
+++ app/[locale]/(dashboard)/layout.tsx
@@ -1,19 +1,19 @@
-import { AppSidebar } from "@/components/app-sidebar"
-import { DynamicBreadcrumb } from "@/components/dynamic-breadcrumb"
-import { Separator } from "@/components/ui/separator"
+import { AppSidebar } from "@/components/app-sidebar";
+import { DynamicBreadcrumb } from "@/components/dynamic-breadcrumb";
+import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from "@/components/ui/sidebar"
-import { setupLocale } from '@/lib/locale-setup';
+} from "@/components/ui/sidebar";
+import { setupLocale } from "@/lib/locale-setup";
 
 export default async function DashboardLayout({
   children,
   params,
 }: {
-  children: React.ReactNode
-  params: Promise<{ locale: string }>
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
   // Use helper para configuración de locale
   await setupLocale(params);
@@ -24,7 +24,7 @@ export default async function DashboardLayout({
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+            <SidebarTrigger className="-ml-1 text-muted-foreground" />
             <Separator
               orientation="vertical"
               className="mr-2 data-[orientation=vertical]:h-4"
@@ -32,10 +32,10 @@ export default async function DashboardLayout({
             <DynamicBreadcrumb />
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex flex-1 flex-col gap-4 p-4 md:px-20 pt-0">
           {children}
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
