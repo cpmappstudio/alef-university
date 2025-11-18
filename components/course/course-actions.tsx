ad8332c1cd97e584c59fbe808a8b6e528e7c5fae
Juan Camilo Narváez <66099723+ulvenforst@users.noreply.github.com>
2025-11-18 16:01:13 -0500

    Comment out ClassEnrollmentImportDialog
    Comment out ClassEnrollmentImportDialog component.

--- components/course/course-actions.tsx
+++ components/course/course-actions.tsx
@@ -19,7 +19,7 @@ export default function CourseActions() {
         }
       />
       <CourseImportDialog />
-      <ClassEnrollmentImportDialog />
+      {/* <ClassEnrollmentImportDialog /> */}
     </div>
   );
 }
